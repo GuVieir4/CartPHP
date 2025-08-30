157405462f9c469905f03e56f42a65b72035f4ba
Gustavo Henrique <160288170+GuVieir4@users.noreply.github.com>
2025-08-29 21:47:54 -0300

    basic for clone

---
+++ Product.php
@@ -0,0 +1,16 @@
+<?php
+
+Class Product {
+    private array $products;
+
+    public function _construct() {
+
+        $this->productsData = [
+            ["productId" => 1, "name" => "Ryzen 5 5500", "amount" => 500]
+        ]
+        
+    }
+}
+
+
+?>
