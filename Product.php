ce6978c677614411c45daa80f2a53a53f5250674
Le0assis <andreluisdasilvareis03@gmail.com>
2025-09-01 19:04:14 -0300

    Implementação da classe produto
    Classe Product feita e comentada para auxiliar na documentação futura

--- Product.php
+++ Product.php
@@ -1,16 +1,20 @@
 <?php
 
-Class Product {
-    private array $products;
+Class Product{                       #classe que define o comportamento de seus objetos usando métodos e modificando seus estados como atributos
+    public int $id;
+    public string $name;              #atributos da classe
+    public float $price;
+    public int $stock;
 
-    public function _construct() {
-
-        $this->productsData = [
-            ["productId" => 1, "name" => "Ryzen 5 5500", "amount" => 500]
-        ]
-        
+    public function __construct(int $id, string $name, float $price, int $stock)  #é chamado automaticamente sempre que um novo objeto da classe for criado
+    {                                                                             #serve para inicializar os atributos do objeto quando ele for criado, em outras
+        $this->id = $id;                                                          #em outras palavras ele define os valores iniciais dos atributos da classe no momento
+        $this->name = $name;                                                      #em que um novo produto for criado.
+        $this->price = $price;
+        $this->stock = $stock;
     }
 }
+
 
 
 ?>
